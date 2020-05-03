d8e721dbbfeee011b5d5ad8e698dbb5faa1f2092
Jon Q <hello@jonquach.com>
2020-05-03 12:31:10 -0400

    Provide init argument for useReducer

--- src/useStateReducer.js
+++ src/useStateReducer.js
@@ -1,12 +1,17 @@
 import { useRef, useReducer } from 'react';
 import { combineStateReducers } from './combineStateReducers';
 
-export function useStateReducer(initialReducer, initialState, stateReducers) {
+export function useStateReducer(
+	initialReducer,
+	initialState,
+	stateReducers,
+	init,
+) {
 	const combinedReducers = combineStateReducers(stateReducers);
 
 	const reducer = useRef((state, action) => {
 		return combinedReducers(initialReducer({ ...state }, action), action);
 	}).current;
 
-	return useReducer(reducer, initialState);
+	return useReducer(reducer, initialState, init);
 }
